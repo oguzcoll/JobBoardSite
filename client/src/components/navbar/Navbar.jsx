f69fba32893eba8efa47371a759b78c2c36ad696
Oguz Colak <oguzcolak2000@gmail.com>
2023-04-11 21:43:04 +0300

    Navbar is completed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./navbar.css";
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="left">
+          <div className="text">UWannaJob</div>
+        </div>
+        <div className="middle">
+          <a href="/">
+            <span>Home</span>
+          </a>
+          <a href="/">
+            <span>About</span>
+          </a>
+          <a href="/">
+            <span>Salary</span>
+          </a>
+        </div>
+        <div className="right">
+          <button className="login">Login</button>
+          <button className="register">Register</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;

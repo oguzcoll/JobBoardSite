776d19314eca3015f95089e0cee544af15c8d7a7
godsoftware <resul_2001_51@outlook.com>
2023-04-12 20:28:49 +0300

    RegisterPage

--- client/src/pages/employeeRegister/EmployeeRegister.jsx
+++ client/src/pages/employeeRegister/EmployeeRegister.jsx
@@ -1,110 +1,58 @@
-import React, { useState } from "react";
+import React from 'react'
 import "./employeeRegister.css";
+import signimg from './signimg.png';
 
-function EmployeeRegister() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [graduatedDepartment, setGraduatedDepartment] = useState("");
-  const [currentJob, setCurrentJob] = useState("");
-  const [birthDate, setBirthDate] = useState("");
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
 
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleGraduatedDepartmentChange = (event) => {
-    setGraduatedDepartment(event.target.value);
-  };
-
-  const handleCurrentJobChange = (event) => {
-    setCurrentJob(event.target.value);
-  };
-
-  const handleBirthDateChange = (event) => {
-    setBirthDate(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`First Name: ${firstName}`);
-    console.log(`Last Name: ${lastName}`);
-    console.log(`Email: ${email}`);
-    console.log(`Password: ${password}`);
-    console.log(`Graduated Department: ${graduatedDepartment}`);
-    console.log(`Current Job: ${currentJob}`);
-    console.log(`Birth Date: ${birthDate}`);
-  };
+ function EmployeeRegister() {
 
   return (
-    <div className="employee-register-container">
-      <h1>Employee Register</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input
-            type="text"
-            value={firstName}
-            onChange={handleFirstNameChange}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" value={lastName} onChange={handleLastNameChange} />
-        </label>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </label>
-        <label>
-          Graduated Department:
-          <input
-            type="text"
-            value={graduatedDepartment}
-            onChange={handleGraduatedDepartmentChange}
-          />
-        </label>
-        <label>
-          Current Job:
-          <input
-            type="text"
-            value={currentJob}
-            onChange={handleCurrentJobChange}
-          />
-        </label>
-        <label>
-          Birth Date:
-          <input
-            type="date"
-            value={birthDate}
-            onChange={handleBirthDateChange}
-          />
-        </label>
-        <button type="submit">Register</button>
-      </form>
+
+    <div className="employeeregister">
+      
+    
+    <div>
+        <div className="register">
+            <div className="col-1">
+                <h2>Register</h2>
+                <span>Register And Find The Job</span>
+
+                <form id='form' className='forms'>
+                    <input type="text"  placeholder='Name' />
+                    <input type="text" placeholder='Surname' />
+                    <input type="text"  placeholder='Password' />
+                    <input type="text"  placeholder='Confirm password' />
+                    <input type="text" placeholder='BirthDate' />
+                    <input type="text" placeholder='Graduation' />
+                    <input type="text" placeholder='Mobile Number' />
+                    
+
+                   
+                    <button className='signbtn'>Sign In</button>
+                    
+                </form>
+
+
+                
+                
+                <div className="logininfo">Do you have an account?</div>
+                <button className='logbtn'>Log In</button>
+</div>
+
+
+
+            
+            <div className="col-2">
+            <img className='signimg' src={signimg} alt="login" />
+
+            
+                
+            </div>
+
+        </div>
+        </div>
+   
     </div>
   );
-}
-
+};
 export default EmployeeRegister;

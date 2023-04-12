a58ba6e17f0626ceabcdd6dcfbec4b98a0f5b93a
godsoftware <resul_2001_51@outlook.com>
2023-04-12 12:17:21 +0300

    home düzeltildi

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,19 +4,9 @@ import Footer from "../../components/footer/Footer";
 import JobLists from "../../components/jobLists/JobLists";
 import Search from "../../components/search/Search";
 
-
 function Home() {
   return (
-
     <div>
-      <header className="header">
-        <div className="logo">Logo</div>
-        <nav className="nav">
-          <a href="#sallama">Link 1</a>
-          <a href="#sallama1">Link 2</a>
-          <a href="#sallama2">Link 3</a>
-        </nav>
-      </header>
       <Search />
       <JobLists />
       <Footer />

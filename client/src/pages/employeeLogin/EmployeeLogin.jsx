04459e487d38c958ff9c828e1c4906f2944b90c9
muratthe <116306127+muratthe@users.noreply.github.com>
2023-04-13 10:38:33 +0300

    Login Page has been created and fixed

--- client/src/pages/employeeLogin/EmployeeLogin.jsx
+++ client/src/pages/employeeLogin/EmployeeLogin.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 import "./employeeLogin.css";
-function EmployeeLogin() {
-  return <div>Login</div>;
-}
+import logimg from './dream-job.jpg'
 
-export default EmployeeLogin;
+function EmployeeLogin() {
+  return (
+
+    <div className="employeeLogin">
+      
+    
+    <div>
+        <div className="employee-login">
+            <div className="col-1">
+                <h2>Login</h2>
+                <span>Login And Find Your Dream Job</span>
+
+                <form id='form' className='forms'>
+                    <input type="text"  placeholder='Mail Address' />
+                    
+                    <input type="text"  placeholder='Password' />
+                                    
+                    <button className='signbtn'>Sign In</button>
+                    
+                </form>
+
+                <div className="register-info">Don't you have an account?</div>
+                <button className='regbtn'>Register</button>
+</div>
+
+            <div className="col-2">
+            <img className='signimg' src={logimg} alt="login" />
+
+          
+            </div>
+
+        </div>
+        </div>
+   
+    </div>
+  );
+
+};
+
+export default EmployeeLogin;

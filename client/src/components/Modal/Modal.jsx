8fd75a03238154893c4a4104bf771c5f31d5ce65
muratthe <116306127+muratthe@users.noreply.github.com>
2023-04-28 14:48:42 +0300

    fixeddd

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import "./modal.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Modal() {
-    const back = useNavigate();
+  const back = useNavigate();
+  const companyName = "Morabaa Software";
+  const requirements = [
+    "Bachelor's degree in Computer Science or related field",
+    "Minimum of 5 years experience in software engineering",
+    "Proficiency in JavaScript and React",
+    "Strong problem-solving skills",
+    "Excellent written and verbal communication skills"
+  ];
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
         <div className="titleCloseBtn">
           <button
-            
           >
             X
           </button>
         </div>
         <div className="title">
-          <h1>Software Engineering</h1>
+          <h1>{companyName} - Software Engineering</h1>
         </div>
         <div className="body">
-          <p>Information about the job</p>
+          <p>
+          </p>
+          <ul>
+            {requirements.map((req, index) => (
+              <li key={index}>{req}</li>
+            ))}
+          </ul>
         </div>
         <div className="btns">
           <button
